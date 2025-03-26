8bd302a61ccd44677d4ee3fdafc0dc2f4b88ff4b
PENKOV91 <petar9191@icloud.com>
2025-03-26 20:53:15 +0000

    Update config.py

--- core/config.py
+++ core/config.py
@@ -5,6 +5,7 @@ class Settings(BaseSettings):
     DEEPSEEK_API_KEY: str = os.getenv("DEEPSEEK_API_KEY", "")
     GITHUB_API_KEY: str = os.getenv("GITHUB_API_KEY", "")
     WEBPILOT_API_KEY: str = os.getenv("WEBPILOT_API_KEY", "")
+    DEFAULT_CONTENT_LIMIT: int = int(os.getenv("DEFAULT_CONTENT_LIMIT", 1000))
 
     class Config:
         env_file = ".env"
